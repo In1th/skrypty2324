4173c1e696c47ce0c2d80483df7556627f61305b
Mateusz Kruk <krukm634@gmail.com>
2023-12-20 23:43:16 +0100

    frontend setup

--- js/backend/index.js
+++ js/backend/index.js
@@ -4,7 +4,24 @@ const port = process.env.PORT ?? 3001;
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+});
+
+app.get('/products', (req, res) => {
+  //all with or without all param
+
+  //if category param, only get category
+  console.log(req.query)
+  res.send('Hello World!')
 })
+
+app.get('/orders', (req, res) => {
+  //get all orders for user
+})
+
+app.post('/buy', (req, res) => {
+  //validate
+  // order a thing
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
