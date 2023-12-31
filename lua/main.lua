46e68701146cf6f0329b36c62eb6c78721ee4d7d
Mateusz Kruk <krukm634@gmail.com>
2023-12-31 15:00:48 +0100

    Update main.lua

--- lua/main.lua
+++ lua/main.lua
@@ -1,5 +1,21 @@
 lapis = require "lapis"
+import Model from require "lapis.db.model"
+import respond_to, capture_errors from require "lapis.application"
+
+class Products extends Model
 
 class extends lapis.Application
-  "/": =>
-    "Hello world!"
+  [read_products: "/products"]: =>
+    Products\select!
+
+  [new_product: "/product/new"]: =>
+    "HI!"
+
+  [product: "/product/:id"]: respond_to {
+    POST: capture_errors =>
+      "HI!"
+    
+    DELETE: =>
+      "HI!"
+
+  }
